0df86f86bcb79d0afa54807010cdfd1290c292fd
Gabriel Puebla <gabrielpuebla16@gmail.com>
2024-01-12 19:04:32 -0300

    javaScript updates

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,7 @@
 let yearElement = document.querySelector('#year');
 yearElement.innerHTML = '2024';
+
+// Last Modification: 
+
+let nLastModif = Date(document.lastModified);
+document.querySelector('#lastModification').innerHTML = `Last Modification: ${document.lastModified}`;
