d20ac9ff4c8a8628a4117830818a038ba302c80e
Gabriel Puebla <gabrielpuebla16@gmail.com>
2024-01-20 21:29:58 -0300

    adding hamburguer menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -5,3 +5,13 @@ yearElement.innerHTML = '2024';
 
 let nLastModif = Date(document.lastModified);
 document.querySelector('#lastModification').innerHTML = `Last Modification: ${document.lastModified}`;
+
+const mainnav = document.querySelector('nav')
+const hambutton = document.querySelector('#menu');
+const navbar = document.querySelector('nav');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+    navbar.classList.toggle('noDisplay');
+});
