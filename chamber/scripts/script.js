5d0f506e671f774b02c1ec0332227a12d30525e3
Gabriel Puebla <gabrielpuebla16@gmail.com>
2024-01-29 19:19:52 -0300

    updating img home page chamber

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,13 @@
+let yearElement = document.querySelector('#year');
+yearElement.innerHTML = '2024';
+
+// Last Modification: 
+
+let nLastModif = Date(document.lastModified);
+document.querySelector('#lastModification').innerHTML = `Last Modification: ${document.lastModified}`;
+
+// Navigation Menu
+
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
