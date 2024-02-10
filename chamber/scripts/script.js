4ad6b5d8572fb55867b5b3c37f631a07aee83940
GPuebla <gabrielpuebla16@gmail.com>
2024-02-10 07:10:01 -0300

    last modifications chamber-discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,6 +1,10 @@
 let yearElement = document.querySelector('#year');
 yearElement.innerHTML = '2024';
 
+const welcMsg = document.querySelector("#welc-msg");
+const currentDate = Date.now();
+const msToDays = 84600000;
+
 // Last Modification: 
 
 let nLastModif = Date(document.lastModified);
@@ -16,3 +20,22 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+// Visits
+let lastLogStored = Number(window.localStorage.getItem("lastLog")) || 0;
+
+let daysElapsed = (currentDate - lastLogStored) / msToDays;
+
+	if (daysElapsed !== 0 && daysElapsed< 1) {
+		welcMsg.textContent = `Back so soon! Awesome!`;
+	} 
+	else if (daysElapsed == 0 ) {
+		welcMsg.textContent = `Welcome! Let us know if you have any questions`;
+	} 
+	else{
+		welcMsg.textContent = `You last visited ${daysElapsed.toFixed(0)} days ago `;
+	}
+	
+
+localStorage.setItem("daysElapsedStored", daysElapsed);
+localStorage.setItem("lastLog", currentDate);
