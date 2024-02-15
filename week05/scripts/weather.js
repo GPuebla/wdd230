a6a191d288f7a9a391907d652ac3f962e58d2898
GPuebla <gabrielpuebla16@gmail.com>
2024-02-15 19:09:14 -0300

    Weather activity - week 5

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,8 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon'); 
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/lat=49.75134768338387&lon=6.635646868533408&appid=ecd25cadf3f310d7fdf3c83260934125';
-
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ecd25cadf3f310d7fdf3c83260934125';
 
 
 async function apiFetch() {
@@ -11,7 +10,7 @@ async function apiFetch() {
         const response = await fetch (url); 
         if (response.ok) {
             const data = await response.json (); 
-            console.log(data); // testing only
+            console.table(data.weather[0].icon); // testing only
             // displayResults (data); // uncomment when ready } 
         }else {
             throw Error (await response.text());
