7b308936063d78d90ccffb294f7c8beacab1d20a
Gabriel Puebla <gabrielpuebla16@gmail.com>
2024-02-20 06:44:19 -0300

    updating directory

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,22 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    const url = '#';
-    const cards = document.querySelector('#cards');
+    const url = 'https://gpuebla.github.io/wdd230/chamber/data/members.json';
+    const cards = document.querySelector('#members-container');
 
-    const getProphetData = async () => {
-    const response = await fetch(url)
-    data = await response.json()
+    const getMemberData = async () => {
+        const response = await fetch(url)
+        data = await response.json()
 
-    displaymembers(data.members);
-    console.table(data.members) // test
+        displayMembers(data.members);
+        console.table(data.members) // test
+
     };
 
-    const displaymembers = (members) => {
 
+    const displayMembers = (members) => {
+    
+        members.forEach( member => {
+    
+            let card = document.createElement('section');
+            let logo = document.createElement('img');
+
+
+            let companyName = document.createElement('h2');
+           
+    
+            let address = document.createElement('p');
+            address.textContent = `Address: ${member.address}`;
+            
+            let link = document.createElement('p');
+            link.textContent = `${member.link}`;           
+    
+            companyName.textContent = `${member.company_name}`;
+            logo.setAttribute('src', member.logo);
+            logo.setAttribute('alt', `logo of ${member.company_name}`);
+            logo.setAttribute('loading', 'lazy');
+            logo.setAttribute('width', '200');
+            logo.setAttribute('height', '400');
+    
+    
+            card.appendChild(logo);
+            card.appendChild(companyName); 
+            card.appendChild(address);
+            card.appendChild(link);
+            
+    
+            cards.appendChild(card);
+    
+        });
     }
 
-
-
-    getProphetData();
+    getMemberData();
 
 });
